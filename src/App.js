37c3b58cb0f5579bb7eaed1dabaccb1d53e01a83
soufo.25 <nteyouyousoufo>
2022-06-26 13:17:43 +0200

    my-profile

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import React from 'react';
 import './App.css';
 import FullName from './Components/Profile/FullName';
 import Address from './Components/Profile/Address';
-import ProfilePhoto from './Components/Profile/ProfilePhoto';
+import ProfilePhoto from'./Components/Profile/ProfilePhoto';
 
 
 function App() {
-  const name = "Nteyou Nkah"
   return (
   <div className='App'>
     <>
     <ProfilePhoto />
     <FullName />
     <Address />
-    
     </>    
     </div>
     
--- src/Components/Profile/FullName.js
+++ src/Components/Profile/FullName.js
@@ -7,8 +7,8 @@ function FullName() {
   return (
     <div>
         <>
-        <h1>First Name:Nteyou</h1>
-        <h1>Last Name: Yousouf</h1>
+        <h1>First Name:<span>Nteyou</span></h1>
+        <h1>Last Name:<span> Yousouf</span></h1>
         </>
     </div>
   )
